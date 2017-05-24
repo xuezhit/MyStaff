c8707df7d13310922041bd70d64246d27f70f0ab
xuezhitan <791890932@qq.com>
2017-05-24 16:33:51 +0800

    reset

--- app/src/main/java/com/example/hasee/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hasee/myapplication/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.hasee.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(getApplicationContext(),"status",Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_main);
     }
 }
